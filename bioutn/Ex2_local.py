496f0661b94804702e4203ee614477e321dc7ba8
prete <prete@bio-VirtualBox>
2014-06-22 17:21:06 -0300

    converters + resources.

--- bioutn/Ex2_local.py
+++
@@ -1,7 +0,0 @@
-from Bio.Blast import NCBIWWW
-from Bio.Blast.Applications import NcbiblastxCommandline
-
-def Ex2Local():
-    fasta_string = open("ls_orchid.fasta").read();
-    blastx_cline = NcbiblastxCommandline(query="opuntia.fasta", db="nr", evalue=0.001,outfmt=5, out="opuntia.xml")
-    stdout, stderr = blastx_cline()
